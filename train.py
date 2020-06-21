45ac0d1f9b322920f8dec39b4b3b0a7e2b453e63
aditya sharma <adityas29@outlook.com>
2020-06-21 13:31:11 +0530

    train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+import os, subprocess
+
+pid = subprocess.run('pgrep -f bin/rasa', shell=True, check=True, stdout=subprocess.PIPE).stdout.decode()
+
+pids = pid.split('\n')[:-2]
+kill_pid = "kill -9 "
+for id in pids:
+	kill_pid += id + " "
+
+print(pids)
+
+if len(pids) > 0:
+	subprocess.run(kill_pid, shell=True)
+
+# train the model
+
+subprocess.run('rasa train', shell= True)
