ee335f2e9acf755646067748b9d7ead986d204ed
Aditya Sharma <31154243+Adityashar@users.noreply.github.com>
2020-06-21 13:55:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,20 +3,23 @@ import os, subprocess
 #activate env
 #move to dir
 
-pid = subprocess.run('pgrep -f bin/rasa', shell=True, check=True, stdout=subprocess.PIPE).stdout.decode()
+def main():
+	pid = subprocess.run('pgrep -f bin/rasa', shell=True, check=True, stdout=subprocess.PIPE).stdout.decode()
 
-pids = pid.split('\n')[:-2]
-kill_pid = "kill -9 "
-for id in pids:
-	kill_pid += id + " "
+	pids = pid.split('\n')[:-2]
+	kill_pid = "kill -9 "
+	for id in pids:
+		kill_pid += id + " "
 
-print(pids)
+	print(pids)
 
-if len(pids) > 0:
-	subprocess.run(kill_pid, shell=True)
+	if len(pids) > 0:
+		subprocess.run(kill_pid, shell=True)
 
-# train the model
+	# train the model
+	subprocess.run('rasa train', shell= True)
+	# run the shell
+	subprocess.run('rasa run -m models --enable-api --cors "*" --debug', shell=True)
 
-subprocess.run('rasa train', shell= True)
-
-subprocess.run('rasa run -m models --enable-api --cors "*" --debug', shell=True)
+if __name__==__main__:
+	main()
