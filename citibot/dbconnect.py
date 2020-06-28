63399cf6127d93c2913b619730b9de62050bd9bf
aditya sharma <adityas29@outlook.com>
2020-06-28 17:22:05 +0530

    voice integrated bot

--- citibot/dbconnect.py
+++ citibot/dbconnect.py
@@ -1,56 +1,33 @@
 import traceback
 import mysql.connector
+#systemctl status mysql.service
+#sudo mysql -u root -ppassword
+def query_formation(entities, table:str):
 
-def query_formation(entities, table:str, intent:str):
+    query = "SELECT * FROM {} WHERE".format(table)
+    for e in entities:
+        if e['extractor'] == 'CRFEntityExtractor':
+            query += " {} = {} and".format(e['entity'], e['value'])
+    # print(query)
+    return query[:-3]
 
-    query = "SELECT " + intent + " FROM " + table
-    # if(intent == 'pending_payment'):
-    #     query += " Pending Amount FROM " 
-    # elif(intent == 'payment_status'):
-    #     query += " Payment Status FROM "
-
-    cnt = 0
-    for dic in entities:
-        if cnt == 0:
-            query += " WHERE "
-            cnt = cnt + 1
-        else:
-            query += " AND "
-
-
-        query += dic['entity']
-        query += " = "
-        query += dic['value']
-
-        # if(dic['entity'] == 'account_id'):
-        #     query += "Account id = " 
-        #     query += dic['value']
-
-        # if(dic['entity'] == 'legal_entity'):
-        #     query += "Legal Entity = " 
-        #     query += dic['value']
-
-        # if(dic['entity'] == 'client_name'):
-        #     query += "Client Name = " 
-        #     query += dic['value']
-
-    return query
-
-def getData(query:str):
+def getData(query:str, table):
 
         try:
             mydb = mysql.connector.connect(
                 host="localhost",
                 user="root",
-                passwd="",
-                database=""
+                passwd="password",
+                database="Rasadatabases"
                 )
-
             cursor = mydb.cursor()
             cursor.execute(query)
 
             results = cursor.fetchall()
-            return results
+            # features = cursor.execute("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='Rasadatabases' AND `TABLE_NAME`={}".format(table)).fetchall()
+            cursor.execute("show columns from {}".format(table))
+            features = cursor.fetchall()
+            return results, features
         except:
             print("Error occured while connecting to database or fetching data from database. Error Trace: {}".format(traceback.format_exc()))
             return []
